cd284e962036854e8cd83fa472bd740e9ac6d2a6
unknown <pakelvi0707@gmail.com>
2022-02-08 17:01:34 +0300

    page support-m

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+
+let select = function () {
+  let selectHeader = document.querySelectorAll('.select__header');
+  let selectItem = document.querySelectorAll('.select__item');
+
+  selectHeader.forEach(item => {
+      item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+      item.addEventListener('click', selectChoose)
+  });
+
+  function selectToggle() {
+      this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+      let text = this.innerText,
+          select = this.closest('.select'),
+          currentText = select.querySelector('.select__current');
+      currentText.innerText = text;
+      select.classList.remove('is-active');
+
+  }
+
+};
+
+
+select();
+
+
+
+
 let reviewSlider = new Swiper('.reviews__content', {
     slidesPerView: 1.5,
     speed: 800,
@@ -154,3 +188,7 @@ const seeAlsoSlider = new Swiper('.seeAlso__slider', {
       }
   },
 });
+
+
+
+
